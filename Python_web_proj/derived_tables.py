adc5fd7a44155a6b9c7d10167f69040853f5cc17
hishiro <123607245+drlyngrc@users.noreply.github.com>
2023-12-02 11:41:06 +0800

    Added flash messages

--- Python_web_proj/derived_tables.py
+++ Python_web_proj/derived_tables.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 class DerivedTables:
     def __init__(self, read_instance):
         self.read_instance = read_instance
 
     def householdinfo(self, request):
+        householdno = ''  # Default value for rendering
+        householdinfo = []  # Default value for rendering
+
         if request.method == "POST":
             if "action" in request.form and request.form["action"] == "submit":
                 householdno = request.form["householdno"]
                 householdinfo = self.read_instance.read_householdinfo(householdno)
+                if not householdinfo:
+                    flash('No household found for the given number.', 'error')
                 return render_template("householdinfo.html", householdinfo=householdinfo)
 
         return render_template("householdinfo.html", householdinfo=[])
 
     def incomerecord(self, request):
-        incomerecord = []  
+        householdno = ''
+        incomerecord = []  # Default value for rendering
+
         if request.method == "POST":
             if "action" in request.form and request.form["action"] == "submit":
                 householdno = request.form["householdno"]
                 incomerecord = self.read_instance.read_incomerecord(householdno)
+                if not incomerecord:
+                    flash('No records found for the given household number.', 'error')
 
         return render_template("incomerecord.html", incomerecord=incomerecord)
 

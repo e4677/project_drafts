c6556d3597bcb1399e7c7b4b9746d7421654ed35
John Andrei Barican <123534225+e4677@users.noreply.github.com>
2023-12-03 22:47:48 +0800

    Update main.py

--- Python_web_proj/main.py
+++ Python_web_proj/main.py
@@ -17,6 +17,7 @@ from student import Student
 from derived_tables import DerivedTables
 from adminappointment import adminappointment
 from appointment import Appointment
+from registration import save_file, handle_accregistration
 
 app = Flask(__name__)
 app.secret_key = 'BOSS'
@@ -36,84 +37,11 @@ student_instance = Student(read_instance, delete_instance, update_instance)
 derived_tables_instance = DerivedTables(read_instance)
 appointment_instance = Appointment(read_instance, delete_instance, update_instance)
 
-def save_file(file):
-    upload_folder = "uploads"
 
-    if not os.path.exists(upload_folder):
-        os.makedirs(upload_folder)
-
-    filename = os.path.join(upload_folder, secure_filename(file.filename))
-    file.save(filename)
-    return filename
-
-
-
-
-###############################################################################################
 
 @app.route("/accregistration", methods=["GET", "POST"])
-def accregistration():
-    print(request.form)
-    if request.method == "POST":
-        if "action" in request.form:
-            if request.form["action"] == "create":
-                barangayid = request.form["barangayid"]
-                category = request.form["category"]
-                lastname = request.form["lastname"]
-                firstname = request.form["firstname"]
-                middlename = request.form["middlename"]
-                sex = request.form["sex"]
-                birthdate = request.form["birthdate"]
-                birthplace = request.form["birthplace"]
-                religion = request.form["religion"]
-                civilstat = request.form["civilstat"]
-                citizenship = request.form["citizenship"]
-                voterprecinct = request.form["voterprecinct"]
-                contactno = request.form["contactno"]
-                employmentstatus = request.form["employmentstatus"]
-                monthlyincome = request.form["monthlyincome"]
-                yearlevel = request.form["yearlevel"]
-                school = request.form["school"]
-                academicyear = request.form["academicyear"]
-                scholarship = request.form["scholarship"]
-                houseno = request.form["houseno"]
-                zone = request.form["zone"]
-                streetname = request.form["streetname"]
-                city = request.form["city"]
-                province = request.form["province"]
-                bmiclassification = request.form["bmiclassification"]
-                medicalcondition = request.form["medicalcondition"]
-                covidvaccinated = request.form["covidvaccinated"]
-                maintenancemedicine = request.form["maintenancemedicine"]
-                physicalfitness = request.form["physicalfitness"]
-                profile_picture = request.files['profile_picture']
-                saved_filepath = save_file(profile_picture)
-                if saved_filepath:
-                    create_instance.create_residentinfo(barangayid, category, lastname, firstname, middlename, sex, birthdate,
-                                                        birthplace, religion, civilstat, citizenship, voterprecinct, contactno,
-                                                        saved_filepath)
-
-                # Create instances for addressinfo, medicalinfo, adultinfo, studentinfo based on the selected category
-                if category == "Adult":
-                    create_instance.create_adultinfo(request.form["adult_barangayid"], employmentstatus, monthlyincome)
-                    create_instance.create_addressinfo(request.form["address_barangayid"], houseno, zone, streetname, city, province)
-                    create_instance.create_medicalinfo(request.form["medical_barangayid"], bmiclassification, medicalcondition, covidvaccinated, 
-                                                    maintenancemedicine, physicalfitness)
-
-                elif category == "Student":
-                    create_instance.create_studentinfo(request.form["student_barangayid"], yearlevel, school, academicyear, scholarship)
-                    create_instance.create_addressinfo(request.form["address_barangayid"], houseno, zone, streetname, city, province)
-                    create_instance.create_medicalinfo(request.form["medical_barangayid"], bmiclassification, medicalcondition, covidvaccinated, 
-                                                    maintenancemedicine, physicalfitness)
-                elif category == "Others":
-                    create_instance.create_addressinfo(request.form["address_barangayid"], houseno, zone, streetname, city, province)
-                    create_instance.create_medicalinfo(request.form["medical_barangayid"], bmiclassification, medicalcondition, covidvaccinated, 
-                                                    maintenancemedicine, physicalfitness)
-
-                
-                return redirect(url_for('accregistration'))
-
-    return render_template("accregistration.html", accregistration=accregistration)
+def accregistration_route():
+    return handle_accregistration(request, create_instance)
 
 ###############################################################################################
 
